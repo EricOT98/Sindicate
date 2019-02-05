6879e92e8c18ec171f2adf197eb6f62422bf6b35
C00204864 <C00204864@itcarlow.ie>
2019-02-05 11:09:15 +0000

    Added Box2D

--- ARGO_Team_D/ARGO_Team_D/Game.cpp
+++ ARGO_Team_D/ARGO_Team_D/Game.cpp
@@ -82,7 +82,7 @@ void Game::render()
 		SDL_Log("Could not create a renderer: %s", SDL_GetError());
 	}
 
-	SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 255);
+	SDL_SetRenderDrawColor(m_renderer, 255, 0, 0, 255);
 
 	SDL_RenderClear(m_renderer);
 
