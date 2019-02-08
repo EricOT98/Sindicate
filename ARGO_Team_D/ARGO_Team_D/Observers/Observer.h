b3560ca894b6114a88ff89463a4c725cc09d40ab
Dylan Murphy <c00204772@itcarlow.ie>
2019-02-08 13:56:21 +0000

    Observer (#27)
    * Base setup
    
    * Added base system
    
    * Basic Image resource handler
    
    * Sound resources added
    
    * Image returns texture instead of surface
    
    * Loading from json added
    
    * Added resource checker
    
    * Vector API implemented
    
    * Virtual factory added
    
    * Cleanup
    
    * Added Interface classes for subjects and observers

---
+++ ARGO_Team_D/ARGO_Team_D/Observers/Observer.h
@@ -0,0 +1,15 @@
+#ifndef OBSERVER_H
+#define OBSERVER_H
+
+/// <summary>
+/// Observer Interface class.
+/// </summary>
+class Observer {
+public:
+	/**
+	* Update the observer.
+	*/
+	virtual void update() = 0;
+
+};
+#endif
---
+++ ARGO_Team_D/ARGO_Team_D/Observers/Subject.h
@@ -0,0 +1,30 @@
+#ifndef SUBJECT_H
+#define SUBJECT_H
+
+#include "Observer.h"
+
+/// <summary>
+/// Subject Interface class.
+/// </summary>
+class Subject {
+public:
+
+	/**
+	* Register an observer
+	* @param observer the observer object to be registered
+	*/
+	virtual void registerObserver(Observer *observer) = 0;
+
+	/**
+	* Unregister an observer
+	* @param observer the observer object to be unregistered
+	*/
+	virtual void removeObserver(Observer *observer) = 0;
+
+	/**
+	* Notify all the registered observers when a change happens
+	*/
+	virtual void notifyObservers() = 0;
+
+};
+#endif
